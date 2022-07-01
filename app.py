b67f65da2b5a54aeec6a4640c4450ffadc6f971f
Raja Haikal <72386335+rjhaikal@users.noreply.github.com>
2022-07-01 16:30:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World form Btech CX Team!! Update Content Testing '
+    return 'Hello World form Btech CX Team!! Update Content Tesss'
